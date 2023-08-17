31b0bd5f90b241a205406e2983d18f6482235117
Bruno Rodrigues <bytebruno@gmail.com>
2023-08-17 19:23:18 +0100

    feat: ensure main-layout has a wrapper class and router-outlet

--- libs/shared/ui-common/src/lib/shared-ui-common.module.ts
+++ libs/shared/ui-common/src/lib/shared-ui-common.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   declarations: [MainLayoutComponent],
   exports: [MainLayoutComponent],
 })
