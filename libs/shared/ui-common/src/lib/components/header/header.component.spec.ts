8ce4ba7a757f313675535a39e460930574c2841c
Bruno Rodrigues <bytebruno@gmail.com>
2023-08-17 23:33:59 +0100

    test: ensure that the button is hightlited according to current url

--- libs/shared/ui-common/src/lib/components/header/header.component.spec.ts
+++ libs/shared/ui-common/src/lib/components/header/header.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeaderComponent } from './header.component';
 import { MaterialModule } from '../../material.module';
 import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ChangeDetectionStrategy } from '@angular/core';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -9,9 +11,13 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MaterialModule],
+      imports: [MaterialModule, RouterTestingModule],
       declarations: [HeaderComponent],
-    }).compileComponents();
+    })
+      .overrideComponent(HeaderComponent, {
+        set: { changeDetection: ChangeDetectionStrategy.Default },
+      })
+      .compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
@@ -26,5 +32,27 @@ describe('HeaderComponent', () => {
     const buttons = fixture.debugElement.queryAll(By.css('.mdc-button__label'));
     expect(buttons[0].nativeElement.innerHTML.trim()).toBe('Photos');
     expect(buttons[1].nativeElement.innerHTML.trim()).toBe('Favorites');
+  });
+
+  it('should highlight first button if url is /photos', () => {
+    jest
+      .spyOn(component, 'currentUrl', 'get')
+      .mockImplementation(() => '/photos');
+    fixture.detectChanges();
+
+    const buttons = fixture.debugElement.queryAll(By.css('button'));
+    expect(buttons[0].classes['mat-accent']).toBe(true);
+    expect(buttons[1].classes['mat-accent']).toBe(undefined);
+  });
+
+  it('should highlight second button if url is /favorites', () => {
+    jest
+      .spyOn(component, 'currentUrl', 'get')
+      .mockImplementation(() => '/favorites');
+    fixture.detectChanges();
+
+    const buttons = fixture.debugElement.queryAll(By.css('button'));
+    expect(buttons[0].classes['mat-accent']).toBe(undefined);
+    expect(buttons[1].classes['mat-accent']).toBe(true);
   });
 });
