f697f8e4dbf79801b3398dbc0053eb88d40f4d20
Bruno Rodrigues <bytebruno@gmail.com>
2023-08-17 23:45:50 +0100

    feat: add favorites route

--- libs/photos/feature-photos/src/lib/photos-feature-photos.routing.module.ts
+++ libs/photos/feature-photos/src/lib/photos-feature-photos.routing.module.ts
@@ -1,11 +1,16 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { PhotosComponent } from './screens/photos/photos.component';
+import { FavoritesComponent } from './screens/favorites/favorites.component';
 
 export const APP_ROUTES: Routes = [
   {
     path: 'photos',
     component: PhotosComponent,
+  },
+  {
+    path: 'favorites',
+    component: FavoritesComponent,
   },
 ];
 
