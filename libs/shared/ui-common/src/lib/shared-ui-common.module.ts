514cd0113b3c034f57e27cd839117df525f030d3
Bruno Rodrigues <bytebruno@gmail.com>
2023-08-17 21:45:50 +0100

    feat: add header component

--- libs/shared/ui-common/src/lib/shared-ui-common.module.ts
+++ libs/shared/ui-common/src/lib/shared-ui-common.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { MaterialModule } from './material.module';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
-  declarations: [MainLayoutComponent],
+  imports: [CommonModule, RouterModule, MaterialModule],
+  declarations: [MainLayoutComponent, HeaderComponent],
   exports: [MainLayoutComponent],
 })
 export class SharedUiCommonModule {}
