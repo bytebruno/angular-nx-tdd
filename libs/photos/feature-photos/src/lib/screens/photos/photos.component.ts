42edce067a1ea03cb0e187fcb92dead62eff9fe6
Bruno Rodrigues <bytebruno@gmail.com>
2023-08-18 20:06:23 +0100

    feat: loading and load more implementation

--- libs/photos/feature-photos/src/lib/screens/photos/photos.component.ts
+++ libs/photos/feature-photos/src/lib/screens/photos/photos.component.ts
@@ -1,17 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { PhotosFacade } from '@angular-nx-tdd/photos/domain';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'photos-photos',
   templateUrl: './photos.component.html',
 })
-export class PhotosComponent implements OnInit {
+export class PhotosComponent implements OnInit, OnDestroy {
   photosList$ = this.photosFacade.photosList$;
+  loaded$ = this.photosFacade.loaded$;
+  loadedSubscription = new Subscription();
+  loaded = false;
 
   constructor(private photosFacade: PhotosFacade) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.load();
+    this.loadedSubscription = this.photosFacade.loaded$.subscribe(
+      (val) => (this.loaded = val)
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.loadedSubscription?.unsubscribe();
   }
 
   load(): void {
@@ -19,6 +30,8 @@ export class PhotosComponent implements OnInit {
   }
 
   onScroll() {
-    console.log('scroll');
+    if (!this.loaded) return;
+    console.log('scrolled');
+    this.photosFacade.loadMore();
   }
 }
