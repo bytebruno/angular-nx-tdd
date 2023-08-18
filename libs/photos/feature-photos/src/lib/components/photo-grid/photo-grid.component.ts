a846bc0f8cf632053fc21e24646351ae5828c9ae
Bruno Rodrigues <bytebruno@gmail.com>
2023-08-18 13:55:18 +0100

    feat: render photo-card list on grid component

--- libs/photos/feature-photos/src/lib/components/photo-grid/photo-grid.component.ts
+++ libs/photos/feature-photos/src/lib/components/photo-grid/photo-grid.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Photo } from '@angular-nx-tdd/photos/domain';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'photos-photo-grid',
   templateUrl: './photo-grid.component.html',
   styleUrls: ['./photo-grid.component.scss'],
 })
-export class PhotoGridComponent {}
+export class PhotoGridComponent {
+  @Input() photos: Photo[] | null = null;
+
+  trackById(_: number, photo: Photo): string {
+    return photo.id;
+  }
+}
