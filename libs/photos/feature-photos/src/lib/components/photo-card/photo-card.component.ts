c5c215dd68d0afea4d6a731e0c5318e2ed665be0
Bruno Rodrigues <bytebruno@gmail.com>
2023-08-18 12:18:20 +0100

    feat: add photo-card component

---
+++ libs/photos/feature-photos/src/lib/components/photo-card/photo-card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'photos-photo-card',
+  templateUrl: './photo-card.component.html',
+  styleUrls: ['./photo-card.component.scss'],
+})
+export class PhotoCardComponent {}
