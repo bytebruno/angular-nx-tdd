301851adb53829478f2d6d0065b2bfc3968b5e88
Bruno Rodrigues <bytebruno@gmail.com>
2023-08-18 13:51:05 +0100

    feat: receive Photo object as an Input of photo-card

--- libs/photos/feature-photos/src/lib/components/photo-card/photo-card.component.ts
+++ libs/photos/feature-photos/src/lib/components/photo-card/photo-card.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Photo } from '@angular-nx-tdd/photos/domain';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'photos-photo-card',
   templateUrl: './photo-card.component.html',
   styleUrls: ['./photo-card.component.scss'],
 })
-export class PhotoCardComponent {}
+export class PhotoCardComponent {
+  @Input() photo: Photo | null = null;
+}
