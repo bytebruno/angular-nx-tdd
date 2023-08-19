6da0eb4f75ce4347f985851e4ec7af7f4d785c49
Bruno Rodrigues <bytebruno@gmail.com>
2023-08-19 11:47:09 +0100

    chore: add fake delay

--- libs/photos/domain/src/lib/+state/photos/photos.effects.ts
+++ libs/photos/domain/src/lib/+state/photos/photos.effects.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { createEffect, Actions, ofType, concatLatestFrom } from '@ngrx/effects';
-import { catchError, map, switchMap } from 'rxjs/operators';
+import { catchError, delay, map, switchMap } from 'rxjs/operators';
 import { of } from 'rxjs';
 import * as PhotosActions from './photos.actions';
 import { PhotosDataService } from '../../infrastructure/photos.data.service';
@@ -12,6 +12,9 @@ export class PhotosEffects {
   loadPhotos$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(PhotosActions.loadPhotos),
+      // I'm using real API calls (but is quite fast), I'm adding the delay just to see better the loading behavior.
+      // Comment the line below if you want to remove it
+      delay(Math.floor(Math.random() * 101) + 200),
       concatLatestFrom(() => this.photosFacade.currentPage$),
       switchMap(([_, currentPage]) =>
         this.photosDataService.load(currentPage).pipe(
