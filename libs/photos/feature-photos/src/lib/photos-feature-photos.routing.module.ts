3c9cb985f8496c0085186f659a1bb11eb59a580a
Bruno Rodrigues <bytebruno@gmail.com>
2023-08-19 01:04:17 +0100

    feat: add photo details route

--- libs/photos/feature-photos/src/lib/photos-feature-photos.routing.module.ts
+++ libs/photos/feature-photos/src/lib/photos-feature-photos.routing.module.ts
@@ -2,6 +2,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { PhotosComponent } from './screens/photos/photos.component';
 import { FavoritesComponent } from './screens/favorites/favorites.component';
+import { PhotoDetailComponent } from './screens/photo-detail/photo-detail.component';
 
 export const APP_ROUTES: Routes = [
   {
@@ -11,6 +12,10 @@ export const APP_ROUTES: Routes = [
   {
     path: 'favorites',
     component: FavoritesComponent,
+  },
+  {
+    path: 'photo/:id',
+    component: PhotoDetailComponent,
   },
 ];
 
