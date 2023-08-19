ebeb8c32eb17bdedd671f79007d2904b3d4970c2
Bruno Rodrigues <bytebruno@gmail.com>
2023-08-19 12:18:37 +0100

    test: photo detail tests dependencies

--- libs/photos/feature-photos/src/lib/screens/photo-detail/photo-detail.component.spec.ts
+++ libs/photos/feature-photos/src/lib/screens/photo-detail/photo-detail.component.spec.ts
@@ -1,5 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PhotoDetailComponent } from './photo-detail.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { PhotosFacade } from '@angular-nx-tdd/photos/domain';
+import { of } from 'rxjs';
+
+const mockPhotosFacade = {
+  photosList$: of([]),
+  loaded$: of(false),
+  currentPage$: of(2),
+  load: () => undefined,
+  saveAsFavorite: () => undefined,
+};
 
 describe('PhotoDetailComponent', () => {
   let component: PhotoDetailComponent;
@@ -7,7 +18,14 @@ describe('PhotoDetailComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [PhotoDetailComponent],
+      providers: [
+        {
+          provide: PhotosFacade,
+          useValue: mockPhotosFacade,
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(PhotoDetailComponent);
